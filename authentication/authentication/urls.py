5231aaf016aa38d4504fe851d767f27e8f1fab45
inductionotg <ritesh2000.sinha@gmail.com>
2020-11-26 06:32:37 +0530

    Define url for home_page_view

--- authentication/authentication/urls.py
+++ authentication/authentication/urls.py
@@ -15,7 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from testApp import views
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('ad/', views.home_page_view),
 ]
