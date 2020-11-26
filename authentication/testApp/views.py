63f41be6a06cc22a4930b45bbe5fb7056372509f
inductionotg <ritesh2000.sinha@gmail.com>
2020-11-26 07:20:39 +0530

    Import @login_required from auth module

--- authentication/testApp/views.py
+++ authentication/testApp/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def home_page_view(request):
     return render(request,'testApp/home.html')
-
+@login_required
 def java_exams_view(request):
     return render(request,'testApp/javaexams.html')
 
