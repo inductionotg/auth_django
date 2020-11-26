06a63aac2f63903f5876606f4cb4745088360f81
inductionotg <ritesh2000.sinha@gmail.com>
2020-11-26 06:42:06 +0530

    Add java_exam_view function

--- authentication/testApp/views.py
+++ authentication/testApp/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 def home_page_view(request):
     return render(request,'testApp/home.html')
+
+def java_exams_view(request):
+    return render(request,'testApp/javaexams.html')
+    
